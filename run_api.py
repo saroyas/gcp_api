b848825204547c03ee1b4b002863722432a0b05e
Shehryar Saroya <shehryar.saroya@gmail.com>
2023-05-20 20:42:25 +0100

    updated reqs

--- run_api.py
+++ run_api.py
@@ -1,3 +1,5 @@
+# sudo pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+
 from flask import Flask, request, jsonify
 import torch
 from typing import Dict, List, Any
